db31eef168ff594af8b4d4821c9d774668eb5f9c
Boris Bochkarev <Boris-Bochkaryov@yandex.ru>
2020-07-06 13:57:50 +0700

    Init commit

---
+++ src/test/java/ru/betry/skills/SkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.betry.skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
